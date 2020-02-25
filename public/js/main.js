44361bb9fb0cd7348dbead8563dab127064e4331
mazuecos3 <oscar3mazuecos@hotmail.com>
2020-02-25 16:52:21 +0100

    Login with boostrap done

--- public/js/main.js
+++ public/js/main.js
@@ -1,7 +1,12 @@
 function init() {
-
+    console.log(document.getElementById("acceder"));
+    document.getElementById("acceder").addEventListener("click", prueba);
     console.log("Iniciadno...")
 }
 
+function prueba() {
+    window.location.replace("http://www.w3schools.com");
+
+}
 // The mother of the lamb.
 window.onload = init;
