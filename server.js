59948d5f82401a5135c1855fe03762d1fabc26a6
Oscar Mazuecos <45294960+mazuecos3@users.noreply.github.com>
2020-03-12 10:05:21 +0200

    +body-parser

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+const bodyParser = require('body-parser')
 app.use(express.json());
+app.use(bodyParser.json());
 
 
 //para que heroku elija el puerto que tenga libre
