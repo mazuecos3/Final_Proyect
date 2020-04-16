641504e3d6eb4929ee93e0902b2a617140a57920
Oscar Mazuecos <45294960+mazuecos3@users.noreply.github.com>
2020-04-16 21:53:46 +0200

    ++

--- public/js/consultas.js
+++ public/js/consultas.js
@@ -13,33 +13,30 @@ module.exports.insert = function consutaInsert(usuario, email, edad, password) {
     var values = {usuario, email, edad, password};
     console.log(values);
     
-    con.connect(function(err) {
-        if (err) throw err;
+   
+     
         con.query('INSERT INTO valenrunner SET ?;', values, function (err, result) {
           if (err) throw err;
           console.log(result);
 
           
         });
-        con.end();
-      });    
+           
 }
 
 
 module.exports.comprobar = function comprobar(usuario, password, res) {
 
- 
-  con.connect(function(err) {
-    if (err) throw err;
-
  //Con esta consulta comprobamos si el usuario esta en la base de datos
+
+
     con.query("SELECT usuario,password FROM valenrunner WHERE usuario LIKE '" + usuario + "' AND password LIKE '"+ password + "';", function (err, result) {
-console.log(result);
+console.log(result.length);
 //console.log(result.length);
 //si no estuviera en la base de datos el result que devuve es 0, entonces con una condición sencilla de si 
 //es mayor que cero(si que está en la base de datos)
 //de lo contrario, saldrá una alerta de que hay que registrarse primero
-       if (result[0].usuario == usuario && result[0].password == password) {
+       if (result.length > 0 ) {
         console.log(result[0].usuario);
         console.log(result[0].password);
         res.redirect('main.html'); 
@@ -48,9 +45,9 @@ console.log(result);
         res.redirect('index.html'); 
        
       }  
-    });
-    con.end();
-  });      
+    }); 
+  
+ 
 }
 
 
