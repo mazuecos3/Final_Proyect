f36dd75429c613cbae386f13e17b71deffab8f00
Oscar Mazuecos <45294960+mazuecos3@users.noreply.github.com>
2020-04-26 13:03:47 +0200

    +

---
+++ public/js/consultas.js
@@ -0,0 +1,59 @@
+
+
+var mysql = require('mysql');
+
+var con = mysql.createConnection({
+  host: "gupoe.com",
+  user: "gupoecom_mazuecos3",
+  password: "minimazu3",
+  database: "gupoecom_valenrunner"
+});
+
+module.exports.insert = function consutaInsert(usuario, email, edad, password) {
+    var values = {usuario, email, edad, password};
+    console.log(values);
+    
+   
+     
+        con.query('INSERT INTO valenrunner SET ?;', values, function (err, result) {
+          if (err) throw err;
+          console.log(result);
+
+          
+        });
+           
+}
+
+
+module.exports.comprobar = function comprobar(usuario, password, res) {
+
+ //Con esta consulta comprobamos si el usuario esta en la base de datos
+
+
+    con.query("SELECT usuario,password FROM valenrunner WHERE usuario LIKE '" + usuario + "' AND password LIKE '"+ password + "';", function (err, result) {
+console.log(result.length);
+//console.log(result.length);
+//si no estuviera en la base de datos el result que devuve es 0, entonces con una condición sencilla de si 
+//es mayor que cero(si que está en la base de datos)
+//de lo contrario, saldrá una alerta de que hay que registrarse primero
+       if (result.length > 0 ) {
+        console.log(result[0].usuario);
+        console.log(result[0].password);
+        res.redirect('main.html'); 
+      } else {
+        console.log("Registrate Primero");
+        res.redirect('index.html'); 
+       
+      }  
+    }); 
+  
+ 
+}
+
+
+/*module.exports = {
+    insert: consutaInsert(params)
+  };
+
+   */
+
