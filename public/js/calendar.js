6797cd1db7e94283b5d87f68cbaf9199c5d23137
Oscar Mazuecos <45294960+mazuecos3@users.noreply.github.com>
2020-05-06 20:06:10 +0200

    ++Greey day in the calendar

--- public/js/calendar.js
+++ public/js/calendar.js
@@ -14,40 +14,30 @@ function dates() {
     divDates[0].innerHTML = "";
    let date = document.createElement("div");
    
-    date.classList.add("week");
+   date.classList.add("week");
     console.log(date);
 for (let i = 1; i <=42; i++) {
     contador++
+  
     num = document.createElement("p");
+    num.dataset.num = contador;
+    //CREATION NUMBERS OF THE CALENDAR
     if (contador < 10) {
         num.innerText = "0"+i; 
     }else{
-        num.innerText = +i; 
+        // IF THEY ARE BIGGER THAN 30 WE HAVE TO START 01 , 02 , etc. And add classlist grey
+        if (contador <= 30) {
+            num.innerText = +i; 
+        } else {
+            num.classList.add("grey");
+            num.innerText = i - 30; 
+           
+        }
     }
-  
-    
     console.log(num);
     date.appendChild(num);
 divDates[0].appendChild(date);
 }
-
-
-    /*divDates[0].innerHTML =   `
-    <div class="first-week"> 01 02 03 04 05 06 07</div>
-    <!-- first week -->
-    <div class="second-week"> 08 09 10 11 12 13 14</div>
-    <!-- second week -->
-    <div class="third-week"> 15 16 17 18 19 20 21</div>
-    <!-- third week -->
-    <div class="fourth-week"> 22 23 24 25 26 27 28</div>
-    <!-- fourth week -->
-    <div class="fifth-week"> 29 30 <span class="grey">01 02 03 04 05</span></div>
-    <!-- fifth week -->
-    <div class="sixt-week"><span class="grey"> 06 07 08 09 10 11 12</span></div>
-    <!-- sixt week -->
-`;*/
-
-
 }
 
 
