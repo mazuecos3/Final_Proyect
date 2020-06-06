957f83d7320dc80e8022c2dbcdc9eea3ce047ee4
mazuecos3 <oscar3mazuecos@hotmail.com>
2020-06-06 21:05:34 +0200

    DEPLOYgit add .git add .

--- public/js/login.js
+++ public/js/login.js
@@ -106,7 +106,7 @@ function IniciarLogin() {
     // HEROKU LINK
     // http://valenrunner.herokuapp.com/comprobar for heroku
     //http://localhost:3000/comprobar for localhost
-    fetch("http://valenrunner.herokuapp.com/comprobar ", {
+    fetch("https://valenrunner.herokuapp.com/comprobar ", {
             headers: {
                 Accept: "application/json",
                 "Content-Type": "application/json",
@@ -160,7 +160,7 @@ function comprobarCookie() {
 
     //http://valenrunner.herokuapp.com/verifyToken for heroku 
     //http://localhost:3000/verifyToken for localhost
-    fetch("http://valenrunner.herokuapp.com/verifyToken", {
+    fetch("https://valenrunner.herokuapp.com/verifyToken", {
             headers: {
                 Accept: "application/json",
                 "Content-Type": "application/json",
--- public/js/profile.js
+++ public/js/profile.js
@@ -22,7 +22,7 @@ function reviseCookie() {
     cookie = getCookieValue("tokenUser");
     //http://valenrunner.herokuapp.com/verifyToken for heroku 
     //http://localhost:3000/verifyToken for localhost
-    fetch("http://valenrunner.herokuapp.com/verifyToken", {
+    fetch("https://valenrunner.herokuapp.com/verifyToken", {
             headers: {
                 Accept: "application/json",
                 "Content-Type": "application/json",
